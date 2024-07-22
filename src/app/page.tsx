bb1d92f7dea7933a054558c98593e21ce5f716cd
arasch-saffari <hello@arasch-saffari.com>
2024-07-22 18:06:49 +0200

    set the min for the graph to 20

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -222,7 +222,7 @@ export default function Home() {
           text: "LAS Mittelwert (dB)",
         },
         beginAtZero: true,
-        min: 25, // Set the minimum value of the y-axis to 25
+        min: 20, // Set the minimum value of the y-axis to 25
       },
     },
   };
